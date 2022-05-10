c935e410f9aa5c288202aaddda0944e659df99dd
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-05-11 00:04:51 +0530

    Update DuplicateUsingArrays.java
    output:-
    
    3
    2

--- DuplicateUsingArrays.java
+++ DuplicateUsingArrays.java
@@ -3,6 +3,7 @@ package vyas.Arrays;
 public class DuplicateUsingArrays {
     public static void main(String[] args) {
         int[] arr = {3,5,4,3,2,2,1};
+        //only applicable for 2 duplicate elements
         for(int i =0; i< arr.length;i++)
         {
             for(int j =i+1;j<arr.length; j++)
