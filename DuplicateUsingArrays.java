0e72f68f1efb24d6458b638888f0cd62b618e16d
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-05-10 23:59:53 +0530

    Create DuplicateUsingArrays.java

---
+++ DuplicateUsingArrays.java
@@ -0,0 +1,18 @@
+package vyas.Arrays;
+
+public class DuplicateUsingArrays {
+    public static void main(String[] args) {
+        int[] arr = {3,5,4,3,2,2,1};
+        for(int i =0; i< arr.length;i++)
+        {
+            for(int j =i+1;j<arr.length; j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
